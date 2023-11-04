65a27224cc6573f44f160e12b87f6d226237a891
Shinichi <fauziilyas36363@gmail.com>
2023-11-04 14:44:38 +0700

    Memperbaiki kode 5. Model, Collection, & Controller

--- applications/coba-laravel/resources/views/posts.blade.php
+++ applications/coba-laravel/resources/views/posts.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">Halaman Blog</h1>
-    
-    @foreach ($posts as $post)
-        <article class="mb-5">
-            <h2>
-                <a href="posts/{{ $post->slug }}">{{ $post->title }}</a>
-            </h2>
-            <p>{{ $post->excerpt }}</p>
-        </article>
+{{-- <h1>Halaman Posts</h1> --}}
+    <article class="mb-5">
+        @foreach ($posts as $post)
+        <h2>
+            <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
+        </h2>
+        <h5>Oleh: {{ $post["author"] }}</h5>
+        <p>{{ $post["body"] }}</p>
+    </article>  
     @endforeach
+
+
 @endsection
