5d13a60a19ce13b989bad8d5911acd9914dccc97
Shinichi <fauziilyas36363@gmail.com>
2023-11-23 16:37:17 +0700

    Menyelesaikan 18. Create Post Form

--- applications/coba-laravel/app/Models/Post.php
+++ applications/coba-laravel/app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    use HasFactory;
+    //Trait
+    use HasFactory, Sluggable;
     
     protected $fillable = [
         'title', 
@@ -52,4 +54,15 @@ class Post extends Model
     {
         return 'slug';
     }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => ['title', 'id']
+            ]
+        ];
+    }
+
+
 }
