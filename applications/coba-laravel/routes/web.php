6d27144d9d0e4299b6ec8a1b9e9b31d7cab212d8
Shinichi <fauziilyas36363@gmail.com>
2023-11-27 00:22:51 +0700

    Menyelesaikan 23. Authorization

--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
@@ -69,6 +70,9 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'check
 ->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)
 ->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')
+->middleware('admin');
 
 
 //Route untuk Halaman Categories
